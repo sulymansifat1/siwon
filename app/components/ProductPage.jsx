24cec34480fa74cd455ab78743dd07115147ecb6
Your Name <you@example.com>
2024-02-12 11:50:51 +0600

    third

--- app/components/ProductPage.jsx
+++ app/components/ProductPage.jsx
@@ -101,7 +101,7 @@ const ProductCard = ({ product }) => {
           </span>
           <a
             href={product.buyLink}
-            className="text-white bg-yellow-500 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            className="text-white bg-yellow-500 hover:bg-orange-300 duration-300  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
           >
             Buy Now
           </a>
